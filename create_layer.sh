2377e6a286d483d0c3d009b9f36e73e950c99032
Joe Mader <jmader@jmader.com>
2021-01-26 11:18:31 -0700

    use a requirements.txt approach

--- create_layer.sh
+++ create_layer.sh
@@ -2,14 +2,22 @@
 
 set -e
 
-echo "creating venv for layer: ${package}==${version}"
+echo "installing packaging utilities"
 yum install -y zip
 amazon-linux-extras install -y python3.8
 python3.8 -m pip install --upgrade pip
 python3.8 -m pip install virtualenv
-python3.8 -m venv "${package}"
-source "${package}/bin/activate"
-pip install "${package}==${version}" -t ./python
+
+echo "creating venv for layer: ${layer_name}"
+python3.8 -m venv "${layer_name}"
+source "${layer_name}/bin/activate"
+
+echo "installing python layer requirements"
+pip install -r /requirements.txt -t ./python
 deactivate
-zip -r "${package}.zip" ./python/
+
+echo "creating archive: ${layer_name}.zip"
+zip -r "${layer_name}.zip" ./python/
+
+echo "done creating archive; exiting container"
 exit
--- package_layer.sh
+++ package_layer.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-package="${1}"
-version="${2}"
-container="${package}_layer"
+set -e
+
+layer_requirements_path="${PWD}/layer_requirements.txt"
+
+if [ ! -f "${layer_requirements_path}" ]; then
+  echo "${layer_requirements_path} does not exist or is not a file"
+  exit 1
+fi
+
+layer_name="${1}"
+destination_path="layers/"
+container="layer-packaging-$(uuid)"
+base_dir=$(cd "$(dirname "$0")"; pwd)
 
 docker run --name "${container}" -it \
-  -e package="${package}" \
-  -e version="${version}" \
-  -v $PWD/create_layer.sh:/create_layer.sh \
+  -e layer_name="${layer_name}" \
+  -v "${layer_requirements_path}":/requirements.txt \
+  -v "${base_dir}/create_layer.sh":/create_layer.sh \
   amazonlinux:2.0.20191016.0 \
   ./create_layer.sh
 
 # back in local machine user shell
 echo "copying layer ZIP from container"
-docker cp "${container}:${package}.zip" layers/
+mkdir -p "${destination_path}"
+docker cp "${container}:${layer_name}.zip" "${destination_path}"
 
 echo "deleting container"
 docker rm "${container}"
