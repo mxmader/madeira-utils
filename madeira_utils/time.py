c3eefc4f10234af1122f6977de4b05ff889d8bd2
Joe Mader <jmader@jmader.com>
2021-02-01 14:02:07 -0700

    time funcs

--- madeira_utils/time.py
+++ madeira_utils/time.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 
 
+def get_days_since(reference_date):
+    round((get_timestamp() - reference_date) / 86400)
+
+
 def get_timestamp():
     return round(datetime.utcnow().timestamp())
 
 
 def get_timestamp_object():
     return datetime.utcnow()
+
+
+def get_year():
+    return get_timestamp_object().year
 
 
 def get_year_start(year):
