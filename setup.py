40e96f600f5579682de78c5406ac1aea707a8d06
Joe Mader <jmader@jmader.com>
2021-02-04 09:39:32 -0700

    move in boiler plate falcon API code, shift to falcon 2.0.0

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
     packages=find_packages(exclude=['*.tests', '*.tests.*']),
     install_requires=[
         'dnspython',
+        'falcon==2.0.0',
         'pyyaml',
         'requests'
     ]
